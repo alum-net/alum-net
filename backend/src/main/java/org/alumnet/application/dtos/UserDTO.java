9c9c6b76df01fb329310a0827e4c30c1bb903d80
gburgos <burgosgerman79@gmail.com>
2025-10-17 19:29:59 -0300

    Configure keycloak auth and modify user creation

--- backend/src/main/java/org/alumnet/application/dtos/UserDTO.java
+++ backend/src/main/java/org/alumnet/application/dtos/UserDTO.java
@@ -1,8 +1,6 @@
 package org.alumnet.application.dtos;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,14 +8,25 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@NotEmpty
-@NotNull
 public class UserDTO {
+    @NotNull(message = "Name cannot be null")
+    @NotEmpty(message = "Name cannot be empty")
     private String name;
+
+    @NotNull(message = "Lastname cannot be null")
+    @NotEmpty(message = "Lastname cannot be empty")
     private String lastname;
+
+    @NotNull(message = "Role cannot be null")
+    @NotEmpty(message = "Role cannot be empty")
     private String role;
+
     @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
 
 }
