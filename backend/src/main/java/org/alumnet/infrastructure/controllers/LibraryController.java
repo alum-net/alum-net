65a0faa54f191f3bfe318cafedd3be505848e6cb
tongawz <gsosa@ciemsa.com.uy>
2025-10-28 16:06:53 -0300

    created httpcode

--- backend/src/main/java/org/alumnet/infrastructure/controllers/LibraryController.java
+++ backend/src/main/java/org/alumnet/infrastructure/controllers/LibraryController.java
@@ -8,6 +8,7 @@ import org.alumnet.application.services.LibraryService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -40,6 +41,6 @@ public class LibraryController {
     public ResponseEntity<ResultResponse<LabelDTO>> createLabel(
            @RequestBody String label) {
         LabelDTO newLabel =  libraryService.createLabel(label);
-        return ResponseEntity.ok(ResultResponse.success(newLabel, "Etiqueta creada exitosamente"));
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResultResponse.success(newLabel, "Etiqueta creada exitosamente"));
     }
 }
