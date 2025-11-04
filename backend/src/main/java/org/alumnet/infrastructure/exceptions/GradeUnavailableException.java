6ebe628a10b945a60bcf4217de38cb323423d8f6
German Burgos <68874865+gerburgos@users.noreply.github.com>
2025-11-03 22:20:28 -0300

    Delete backend/src/main/java/org/alumnet/infrastructure/exceptions/GradeUnavailableException.java

--- backend/src/main/java/org/alumnet/infrastructure/exceptions/GradeUnavailableException.java
+++
@@ -1,7 +0,0 @@
-package org.alumnet.infrastructure.exceptions;
-
-public class GradeUnavailableException extends RuntimeException {
-    public GradeUnavailableException() {
-        super("Notas no disponibles.");
-    }
-}
