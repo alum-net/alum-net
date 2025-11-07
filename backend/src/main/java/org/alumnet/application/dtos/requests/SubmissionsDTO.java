093a40dbe0735a6631003c06030cb090929be717
tongawz <gsosa@ciemsa.com.uy>
2025-11-07 20:56:34 -0300

    task files detail

--- backend/src/main/java/org/alumnet/application/dtos/requests/SubmissionsDTO.java
+++ backend/src/main/java/org/alumnet/application/dtos/requests/SubmissionsDTO.java
@@ -1,4 +1,14 @@
 package org.alumnet.application.dtos.requests;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class SubmissionsDTO {
+    private String studentName;
+    private String studentLastname;
+    private String studentEmail;
+    private String fileName;
+    private String fileUrl;
 }
